8ab0d049318310e3a8c5921907618c52fa7c96b9
MHuiG <616001163@qq.com>
2022-08-29 15:43:57 +0800

    csp

--- gulpfile.js
+++ gulpfile.js
@@ -51,72 +51,75 @@ gulp.task('minify', gulp.parallel(
   minify_js
 ))
 
-// CSP inline hash
-const hashstream = require('inline-csp-hash');
-const crypto = require('crypto');
-const hash = (s) => crypto.createHash("sha256").update(s).digest('base64');
+gulp.task('default', gulp.series('minify'));
 
-function getRandStr(len) {
-  var str = '';
-  for (var i = 0; i < len; i++) {
-    str += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-  }
-  return str;
-}
-const nonce = getRandStr(15);
+// （CSP Level 3 ：Safari 15.4+ Chrome 59+ Firefox 58+ Edge 79+）
+// // CSP inline hash
+// const hashstream = require('inline-csp-hash');
+// const crypto = require('crypto');
+// const hash = (s) => crypto.createHash("sha256").update(s).digest('base64');
 
-// script White list [scripts in event handlers (eg onclick)]. 包含压缩的 inline js
-unsafe_script_list = [
-  "this.media='all';this.onload=null",
-  'this.media="all",this.onload=null',
-  "errorImgAvatar(this)",
-  "errorImgCover(this)",
-  "return false;",
-  "return!1",
-  "history.back()",
-  "history.forward()",
-  "window.location.reload()",
-  "VolantisApp.scrolltoElement(volantis.dom.bodyAnchor)",
-  "volantis.rightmenu.jump('prev')",
-  "volantis.rightmenu.jump('next')"
-]
-// script hash White list
-unsafe_script_hash = ["'sha256-MXV1jvkHrZruEyFEOrQRjKs9WlPZC1V/3RLoKrkoDFQ='"]
+// function getRandStr(len) {
+//   var str = '';
+//   for (var i = 0; i < len; i++) {
+//     str += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+//   }
+//   return str;
+// }
+// const nonce = getRandStr(15);
 
-unsafe_script_list.forEach(e => {
-  unsafe_script_hash.push("'sha256-" + hash(e) + "'")
-});
+// // script White list [scripts in event handlers (eg onclick)]. 包含压缩的 inline js
+// unsafe_script_list = [
+//   "this.media='all';this.onload=null",
+//   'this.media="all",this.onload=null',
+//   "errorImgAvatar(this)",
+//   "errorImgCover(this)",
+//   "return false;",
+//   "return!1",
+//   "history.back()",
+//   "history.forward()",
+//   "window.location.reload()",
+//   "VolantisApp.scrolltoElement(volantis.dom.bodyAnchor)",
+//   "volantis.rightmenu.jump('prev')",
+//   "volantis.rightmenu.jump('next')"
+// ]
+// // script hash White list
+// unsafe_script_hash = ["'sha256-MXV1jvkHrZruEyFEOrQRjKs9WlPZC1V/3RLoKrkoDFQ='"]
 
-gulp.task('csp_hash', () => {
-  return gulp.src(['./public/**/*.html', '!./public/{lib,lib/**}', '!./public/{libs,libs/**}', '!./public/{media,media/**}'])
-    .pipe(hashstream({
-      what: 'script:not([type="application/ld+json"])',
-      replace_cb: (s, hashes) => {
-        unsafe_script_hash.push.apply(unsafe_script_hash, hashes);
-        unsafe_script_hash = Array.from(new Set(unsafe_script_hash))
-        return s
-      }
-    }))
-    ;
-});
+// unsafe_script_list.forEach(e => {
+//   unsafe_script_hash.push("'sha256-" + hash(e) + "'")
+// });
 
-gulp.task('csp_replace', () => {
-  return gulp.src(['./public/**/*.html', '!./public/{lib,lib/**}', '!./public/{libs,libs/**}', '!./public/{media,media/**}'])
-    .pipe(hashstream({
-      what: 'script:not([type="application/ld+json"])',
-      replace_cb: (s, hashes) => {
-        unsafe_script_hash = Array.from(new Set(unsafe_script_hash))
-        s = s.replace(/<script.*?>/g, function (match) {
-          return match.replace(/>/g, ` nonce='${nonce}'>`);
-        });
-        s = s.replace(/script-src 'self'[^;]*/, `script-src 'self' https: 'unsafe-hashes' 'nonce-${nonce}' 'strict-dynamic' ` + unsafe_script_hash.join(" "))
-        return s
-      }
-    }))
-    .pipe(gulp.dest('./public'))
-    ;
-});
+// gulp.task('csp_hash', () => {
+//   return gulp.src(['./public/**/*.html', '!./public/{lib,lib/**}', '!./public/{libs,libs/**}', '!./public/{media,media/**}'])
+//     .pipe(hashstream({
+//       what: 'script:not([type="application/ld+json"])',
+//       replace_cb: (s, hashes) => {
+//         unsafe_script_hash.push.apply(unsafe_script_hash, hashes);
+//         unsafe_script_hash = Array.from(new Set(unsafe_script_hash))
+//         return s
+//       }
+//     }))
+//     ;
+// });
 
-gulp.task('csp', gulp.series('csp_hash', 'csp_replace'));
+// gulp.task('csp_replace', () => {
+//   return gulp.src(['./public/**/*.html', '!./public/{lib,lib/**}', '!./public/{libs,libs/**}', '!./public/{media,media/**}'])
+//     .pipe(hashstream({
+//       what: 'script:not([type="application/ld+json"])',
+//       replace_cb: (s, hashes) => {
+//         unsafe_script_hash = Array.from(new Set(unsafe_script_hash))
+//         s = s.replace(/<script.*?>/g, function (match) {
+//           return match.replace(/>/g, ` nonce='${nonce}'>`);
+//         });
+//         s = s.replace(/script-src 'self'[^;]*/, `script-src 'self' https: 'unsafe-hashes' 'nonce-${nonce}' 'strict-dynamic' ` + unsafe_script_hash.join(" "))
+//         return s
+//       }
+//     }))
+//     .pipe(gulp.dest('./public'))
+//     ;
+// });
 
-gulp.task('default', gulp.series('minify', 'csp'));
+// gulp.task('csp', gulp.series('csp_hash', 'csp_replace'));
+
+// gulp.task('default', gulp.series('minify', 'csp'));
